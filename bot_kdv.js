175385ca0276d16d7640c87d9509aafa118b1b8d
Сербин Кирилл <serbin_kirill1807@mail.ru>
2025-08-05 11:08:27 +0700

    feat: вместо текста - картинка (список матчей) feat: актуальная таблица feat: перезапустить бота feat: Ближайший матч feat: добавил ссылок на себя

--- bot_kdv.js
+++ bot_kdv.js
@@ -1,5 +1,7 @@
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -11,41 +13,73 @@ const messageStore = {
     "Привет, это информационный бот ФК КДВ! Скоро здесь будет больше информации!\nА пока подпишись на наш паблик - @boroda_tomsk_youtube",
 };
 
-const matches = {
-  matches1:
-    "✈️ | 7 августа (чт) 16:30 тск // Анри (Владивосток) - ФК КДВ // Кубок России",
-  matches2: "✈️ | 10 августа (вск) // Амкар (Пермь) - ФК КДВ // 16-й тур",
-  matches3: "🏠 | 17 августа (вск) // ФК КДВ - Динамо (Барнаул) // 17-й тур",
-  matches4:
-    "✈️ | 24 августа (вск) // Челябинск-2 (Челябинск) - ФК КДВ // 18-й тур",
-  matches5:
-    "🏠 | 31 августа (вск) // ФК КДВ - Оренбург-2 (Оренбург) // 19-й тур",
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextMatch =
+  "✈️ // 7 августа (чт) 16:30 тск \nАнри (Владивосток) - ФК КДВ \nКубок России \n@boroda_tomsk_youtube";
+const textButtonNextMatch = "Ссылка на трансляцию";
+const urlNextMatch =
+  "https://news.sportbox.ru/Vidy_sporta/Futbol/Russian_Cup/spbvideo_NI2227713_translation_Anri___KDV_FONBET_Kubok_Rossii_po_futbolu_sezona_2025_2026_gg";
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+  sendWelcome(chatId);
+});
 
-  bot.sendMessage(chatId, "Добро пожаловать! Нажмите на кнопку из меню", {
+function sendWelcome(chatId) {
+  bot.sendMessage(chatId, "Добро пожаловать! Нажмите на кнопку из меню.", {
     reply_markup: {
-      keyboard: [["Инфо"], ["Список матчей"]],
+      keyboard: [
+        ["Перезапустить бота"],
+        ["Инфо", "Список матчей"],
+        ["Актуальная таблица", "Ближайший матч (+ссылка)"],
+      ],
       resize_keyboard: true,
       one_time_keyboard: false,
     },
   });
-});
+}
 
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
+
+  if (text === "Перезапустить бота") {
+    sendWelcome(chatId);
+  }
 
   if (text === "Инфо") {
     bot.sendMessage(chatId, messageStore.replyText);
   }
 
   if (text === "Список матчей") {
-    bot.sendMessage(
-      chatId,
-      `1. ${matches.matches1} \n2. ${matches.matches2} \n3. ${matches.matches3} \n4. ${matches.matches4} \n5. ${matches.matches5}`
-    );
+    const imagePath = path.join(__dirname, "static", "matches.jpg");
+    bot.sendPhoto(chatId, imagePath, {
+      caption: "📅 Ближайшие матчи ФК КДВ \n@boroda_tomsk_youtube",
+    });
+  }
+
+  if (text === "Актуальная таблица") {
+    const imagePath = path.join(__dirname, "static", "table.png");
+    bot.sendPhoto(chatId, imagePath, {
+      caption:
+        "🍬 Актуальная таблица Leon Лига Б, группа 4 \n@boroda_tomsk_youtube",
+    });
+  }
+
+  if (text === "Ближайший матч (+ссылка)") {
+    bot.sendMessage(chatId, nextMatch, {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: textButtonNextMatch,
+              url: urlNextMatch,
+            },
+          ],
+        ],
+      },
+    });
   }
 });
